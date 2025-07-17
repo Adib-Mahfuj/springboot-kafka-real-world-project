cc5107673e78dc4558f5e7a2a2c3692ce92f5327
Adib-Mahfuj <adibmahfuj420@gmail.com>
2025-07-17 10:51:36 +0600

    - Save Wikimedia Data into Database - Refactor Code for Remove Hard Coded Values(Topic Name)

--- kafka-consumer-database/src/main/java/com/adib/springboot/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/com/adib/springboot/KafkaDatabaseConsumer.java
@@ -1,5 +1,7 @@
 package com.adib.springboot;
 
+import com.adib.springboot.entity.WikimediaData;
+import com.adib.springboot.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,13 +10,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaDatabaseConsumer {
 
+    private WikimediaDataRepository dataRepository;
+
+    public KafkaDatabaseConsumer(WikimediaDataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
 
     @KafkaListener(
-            topics = "wikimedia_recentChange",
-            groupId = "myGroup123"
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
     )
     public void consume(String eventMessage){
         LOGGER.info(String.format("Event Message Received -> %s",eventMessage));
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setEventData(eventMessage);
+        dataRepository.save(wikimediaData);
     }
 }
